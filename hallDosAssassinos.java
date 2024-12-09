11b6fc4385c99f2d5d31b930a21fa649d84d61c3
Alessandra Faria <139082264+ale-faria@users.noreply.github.com>
2024-12-09 15:44:54 -0300

    Add files via upload

--- hallDosAssassinos.java
+++ hallDosAssassinos.java
@@ -1,5 +1,4 @@
 
-import java.util.List;
 import java.util.Scanner;
 
 public class hallDosAssassinos {
@@ -7,58 +6,69 @@ public class hallDosAssassinos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String assassino, morto;
+        String[] listaDeMortos = new String[20];
+        int[] listaQuantidadeDeMortos = new int[20];
+        String[] listaDeAssassinos = new String[20];
 
-        Assassinos [] listaDeAssassinos = new Assassinos();
-        Mortos [] listaDeMorridos = new Mortos();
-
+        int contaAssassino = 0;
+        int contaMortos = 0;
 
         System.out.println("HALL OF MURDERERS");
+        int x = 0;
+        while (x < 6) {
+            String assassinoNome = sc.next();
+            String mortoNome = sc.next();
 
-        while (sc.hasNext()) {
-            assassino = sc.next();
-            morto = sc.next();
+            listaDeMortos[contaMortos] = mortoNome;
+            contaMortos++;
 
-            listaDeAssassinos.setQuantMortes();
+            // Procurar se o assassino já está na lista
+            int index = -1;
+            for (int i = 0; i < contaAssassino; i++) {
+                if (listaDeAssassinos[i].equals(assassinoNome)) {
+                    index = i;
+                    break;
+                }
+            }
 
+
+            if (index == -1) {
+            // adiciona assassino na lista caso o assassino n exista
+                listaDeAssassinos[contaAssassino] = assassinoNome;
+                listaQuantidadeDeMortos[contaAssassino] = 1;
+                contaAssassino++;
+            } else {
+                // se o assassino já existe, incrementa a lista de quantMortos na posicao certa do assassino
+                listaQuantidadeDeMortos[index]++;
+            }
+            x++;
+        } //fecha while
+
+        String[] resultadoAssassinos = new String[contaAssassino];
+        int[] resultadoAssassinatos = new int[contaAssassino];
+        int resultadoCount = 0;
+
+        for (int i = 0; i < contaAssassino; i++) {
+            boolean encontrado = true;
+            for (int j = 0; j < contaMortos; j++) {
+                if (listaDeAssassinos[i].equals(listaDeMortos[j])) {
+                    encontrado = false;
+                    break;
+                }
+            }
+            if (encontrado) {
+                resultadoAssassinos[resultadoCount] = listaDeAssassinos[i];
+                resultadoAssassinatos[resultadoCount] = listaQuantidadeDeMortos[i];
+                resultadoCount++;
+            }
+
+        }
+        // Imprimir o resultado
+        System.out.println("HALL OF MURDERERS");
+        for (int i = 0; i < resultadoCount; i++) {
+            System.out.println(resultadoAssassinos[i] + " " + resultadoAssassinatos[i]);
         }
 
     }
 
-    sc.close;
-
-}
-
-class Assassinos {
-
-    
-    public int quantMortes;
-
-    public Assassinos() {
-        this.quantMortes = 0;
-    }
-
-    public int getQuantMortes() {
-        return quantMortes;
-    }
-
-    public void setQuantMortes() {
-        this.quantMortes = this.quantMortes + 1;
-    }
-
-}
-
-class Mortos {
-
-    public List<String> mortos;
-    public int quantMorridos;
-
-    public Mortos() {
-        this.quantMorridos = 0;
-    }
-
-    public void setMorto(String nome) {
-        this.mortos.add(nome);
-        quantMorridos++;
-    }
 }
